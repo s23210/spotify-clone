ad9adc904a7f19a65c0c1f5b14895e2fd8867b0d
s23210 <s23210@pjwstk.edu.pl>
2023-06-14 23:57:30 +0200

    Add Modal component

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Modal from "@/components/Modal";
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -15,7 +16,7 @@ const ModalProvider = () => {
 
     return (
         <>
-            Modals
+            <Modal />
         </>
     );
 };
