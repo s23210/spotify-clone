dc5952f1ea7ef0419f41c31e89b005305bdb4a10
s23210 <s23210@pjwstk.edu.pl>
2023-06-14 11:55:23 +0200

    Finish Liked Songs button

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
     image: string;
@@ -22,18 +23,20 @@ const ListItem: React.FC<ListItemProps> = ({
     }
 
     return (
-        <button className="
-            relative
-            group
-            flex
-            items-center
-            rounded-md
-            overflow-hidden
-            gap-x-4
-            bg-neutral-100/10
-            hover:bg-neutral-100/20
-            transition
-            pr-4
+        <button
+            onClick={onClick}
+            className="
+                relative
+                group
+                flex
+                items-center
+                rounded-md
+                overflow-hidden
+                gap-x-4
+                bg-neutral-100/10
+                hover:bg-neutral-100/20
+                transition
+                pr-4
         ">
             <div className="
                 relative
@@ -47,9 +50,26 @@ const ListItem: React.FC<ListItemProps> = ({
                     alt="Image"
                 />
             </div>
-            <p>
+            <p className="font-medium truncate py-5">
                 {name}
             </p>
+            <div className="
+                absolute
+                transition
+                opacity-0
+                rounded-full
+                flex
+                items-center
+                justify-center
+                bg-green-500
+                p-4
+                drop-shadow-md
+                right-5
+                group-hover:opacity-100
+                hover:scale-110
+            ">
+                <FaPlay className="text-black" />
+            </div>
         </button>
     );
 }
