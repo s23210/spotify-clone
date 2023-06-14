8dadfc748098d6b269e8a5f07eb09ed519afe0a5
s23210 <s23210@pjwstk.edu.pl>
2023-06-14 10:38:25 +0200

    Fix bug - typo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -68,7 +68,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 					<Library/>
 				</Box>
 			</div>
-			<main className="h-full dlex-1 overflow-y-auto py-2">
+			<main className="h-full flex-1 overflow-y-auto py-2">
 				{children}
 			</main>
 		</div>
