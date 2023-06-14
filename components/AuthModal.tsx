90f72c4b0badca611541ff64510575c4a28f6b08
s23210 <s23210@pjwstk.edu.pl>
2023-06-15 00:54:23 +0200

    Update AuthModal.tsx

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,16 +1,49 @@
 "use client";
 
+import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import useAuthModal from "@/hooks/useAuthModal";
+import { on } from "events";
 
 const AuthModal = () => {
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter();
+    const { session } = useSessionContext();
+    const { onClose, isOpen } = useAuthModal();
+
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    }
+
     return (
         <Modal
             title="Welcome back"
             description="Login to your account"
-            isOpen
-            onChange={() => {}}
+            isOpen={isOpen}
+            onChange={onChange}
         >
-            Auth modal children!
+            <Auth
+                theme="dark"
+                magicLink
+                providers={['google', 'github', 'facebook', 'discord']}
+                supabaseClient={supabaseClient}
+                appearance={{
+                    theme: ThemeSupa,
+                    variables: {
+                        default: {
+                            colors: {
+                                brand: '#404040',
+                                brandAccent: '#22c55e'
+                            }
+                        }
+                    }
+                }}
+            />
         </Modal>
     );
 }
