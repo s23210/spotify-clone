92f399abb650a7247ea3d46d25dd296123c9043c
s23210 <s23210@pjwstk.edu.pl>
2023-06-15 12:03:36 +0200

    Create UploadModal.tsx

---
+++ components/UploadModal.tsx
@@ -0,0 +1,15 @@
+import Modal from "./Modal"
+
+const UploadModal = () => {
+    return (
+        <Modal
+            title="Upload modal title"
+            description="Upload modal description"
+            isOpen
+            onChange={() => {}}
+        >
+            Upload Content
+        </Modal>
+    );
+}
+export default UploadModal;
