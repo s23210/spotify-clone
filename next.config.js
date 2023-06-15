7225dfece4030bdb5f68750afd2b9de5f3c46d2e
s23210 <s23210@pjwstk.edu.pl>
2023-06-15 14:37:58 +0200

    Add domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "iwlywjefqewnqebwecxl.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
